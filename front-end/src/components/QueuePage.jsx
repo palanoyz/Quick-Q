e4a0e682656e8bffef8d7d9a75d80817ccb25201
Jenesix <acearcadia789@gmail.com>
2024-05-07 22:44:08 +0700

    Queue UI

---
+++ front-end/src/components/QueuePage.jsx
@@ -0,0 +1,33 @@
+const QueuePage = () => {
+    return (
+        <>
+            <div className="text-center mt-12 text-text">
+                <h1 className="text-3xl md:text-5xl font-bold">Suki Teenoi</h1>
+                <div className="mt-12 mb-16">
+                    <h1 className="text-2xl md:text-3xl font-bold mb-2">Your Q (Type: 2 seats)</h1>
+                    <h1 className="text-6xl md:text-8xl text-primary font-bold mb-12">A90</h1>
+                    <p className="text-xl md:text-2xl font-bold">Remaining: 5 Q</p>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 mb-12">
+                    <div className="mt-12">
+                        <h1 className="text-2xl md:text-3xl font-bold mb-2">Type: 2 seats</h1>
+                        <h1 className="text-6xl md:text-8xl text-primary font-bold mb-12">A80</h1>
+                        <p className="text-xl md:text-2xl font-bold">Remaining: 20 Q</p>
+                    </div>
+                    <div className="mt-12">
+                        <h1 className="text-2xl md:text-3xl font-bold mb-2">Type: 4 seats</h1>
+                        <h1 className="text-6xl md:text-8xl text-primary font-bold mb-12">B23</h1>
+                        <p className="text-xl md:text-2xl font-bold">Remaining: 15 Q</p>
+                    </div>
+                    <div className="mt-12">
+                        <h1 className="text-2xl md:text-3xl font-bold mb-2">Type: 10 seats</h1>
+                        <h1 className="text-6xl md:text-8xl text-primary font-bold mb-12">C12</h1>
+                        <p className="text-xl md:text-2xl font-bold">Remaining: 8 Q</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default QueuePage;
