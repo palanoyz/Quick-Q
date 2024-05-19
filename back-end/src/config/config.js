c5f00925cab81e22269717359ac16827ab235f2b
Ariez <kritamet.cho@gmail.com>
2024-05-19 11:36:26 +0700

    reset

--- back-end/src/config/config.js
+++
@@ -1,7 +0,0 @@
-const PORT = 8000;
-const MONGO_URI = "mongodb+srv://kritamet:1234@cluster0.kdvkhdy.mongodb.net/Quick-Q";
-const secret_jwt = "GN000";
-const supabaseUrl = "https://lrxsabvitoecnhgmynmk.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyeHNhYnZpdG9lY25oZ215bm1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4NTUxMzQsImV4cCI6MjAyOTQzMTEzNH0.62r2T8CrtT2BsEV5ve5GoD_hwnEpp5wPoNw5eEasvVE";
-
-module.exports = { PORT, MONGO_URI, secret_jwt, supabaseUrl, supabaseKey };
--- back-end/src/lib/supabase.js
+++
@@ -1,5 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-const { supabaseUrl, supabaseKey } = require("../config/config");
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-module.exports = { supabase };
